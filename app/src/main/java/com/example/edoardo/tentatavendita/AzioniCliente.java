f00dd1f0a9db006b44dd3bfe759cfc6411ce4660
NardiE <nardiedoardo@gmail.com>
2017-05-18 10:57:31 +0200

    creazione attività per le azioni cliente

---
+++ app/src/main/java/com/example/edoardo/tentatavendita/AzioniCliente.java
@@ -0,0 +1,13 @@
+package com.example.edoardo.tentatavendita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AzioniCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_azioni_cliente);
+    }
+}
